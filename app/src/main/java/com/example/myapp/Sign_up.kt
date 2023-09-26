5776f8ff2511b41a44449d67e6d4511d8e041b1f
Saksham Tuteja <tutejasaksham@gmail.com>
2023-09-26 20:01:06 +0530

    add

--- app/src/main/java/com/example/myapp/Sign_up.kt
+++ app/src/main/java/com/example/myapp/Sign_up.kt
@@ -3,9 +3,9 @@ package com.example.myapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Sign_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
- }
+}
