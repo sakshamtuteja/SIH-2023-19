c50d5cd6620e4cb666dd675678d4c1a0ecbdee81
viraj <kumarviraj2003@gmail.com>
2023-09-26 22:15:40 +0530

    lang error

--- app/src/main/java/com/example/myapp/Select_Language.kt
+++ app/src/main/java/com/example/myapp/Select_Language.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class Select_Language : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select_language)
+        setContentView(R.layout.selectlanguage)
     }
 }
