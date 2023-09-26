54a5bcab3c29abe60f1469890eb8de4aed6665b5
Saksham Tuteja <tutejasaksham@gmail.com>
2023-09-26 21:25:24 +0530

    add

--- app/src/main/java/com/example/myapp/Sign_In.kt
+++ app/src/main/java/com/example/myapp/Sign_In.kt
@@ -1,11 +1,59 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import com.example.myapp.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class Sign_In : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        setContentView(binding.root)
+
+        firebase = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, Sign_up::class.java)
+            startActivity(intent)
+        }
+
+        binding.Button.setOnClickListener {
+            binding.progressBar2.visibility = View.VISIBLE
+            val emaili = binding.email.text.toString()
+            val pass = binding.password.text.toString()
+            if (emaili.isNotEmpty() && pass.isNotEmpty()) {
+
+
+                firebaseAuth.signInWithEmailAndPassword(emaili, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+            //binding.progressBar2.visibility = View.GONE
+        }
     }
+/*           override fun onStart(){
+       super.onStart()
+       if(firebaseAuth.currentUser != null){
+           val intent = Intent(this, MainActivity::class.java)
+           startActivity(intent)
+       }
+
+   }*/
 }
--- app/src/main/java/com/example/myapp/Sign_up.kt
+++ app/src/main/java/com/example/myapp/Sign_up.kt
@@ -1,11 +1,49 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.myapp.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Sign_up : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, Sign_In::class.java)
+        startActivity(intent)
+        }
+        binding.button.setOnLongClickListener {
+            binding.progressBar3.visibility = View.VISIBLE
+            val emaili =binding.email.text.toString()
+            val pass = binding.password.text.toString()
+            val confirmpass = binding.repassword.text.toString()
+
+        if (emaili.isNotEmpty() && pass.isNotEmpty() &&confirmpass.isNotEmpty()){
+        if (pass == confirmpass){
+            firebaseAuth.createUserWithEmailAndPassword(emaili,pass).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val intent = Intent(this, Sign_In::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Password is Not matching", Toast.LENGTH_SHORT).show()
+            }
+        } else{
+            Toast.makeText(this, "Empty Fields are not allowed", Toast.LENGTH_SHORT).show()
+        }}
+        }
+        }
     }
-}
