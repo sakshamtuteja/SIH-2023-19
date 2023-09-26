4e67f778193443e4c61c1a8bf46a42704027ad9b
Sourav <kumarraushansingh764@gmail.com>
2023-09-27 04:59:24 +0530

    home activve2

---
+++ app/src/main/java/com/example/myapp/Image.kt
@@ -0,0 +1,12 @@
+package com.example.myapp
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import com.google.android.material.button.MaterialButton
+import java.net.URL
+
+class Image : AppCompatActivity() {
+}
--- app/src/main/java/com/example/myapp/Upload.kt
+++ app/src/main/java/com/example/myapp/Upload.kt
@@ -1,11 +1,22 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Upload : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+
+        val wordopen = findViewById<Button>(R.id.wordbutton)
+        wordopen.setOnClickListener {
+            val Intent = Intent(this,Text::class.java)
+            startActivity(Intent)
+        }
+
+
+
     }
 }
