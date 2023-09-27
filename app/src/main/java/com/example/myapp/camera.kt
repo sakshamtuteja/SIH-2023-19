8e51980722302d9ed9637e705d1d936c3dd4f0f6
Saksham Tuteja <tutejasaksham@gmail.com>
2023-09-27 21:38:04 +0530

    co

--- app/src/main/java/com/example/myapp/camera.kt
+++ app/src/main/java/com/example/myapp/camera.kt
@@ -1,6 +1,5 @@
 package com.example.myapp
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
@@ -10,9 +9,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.createBitmap
 import com.example.myapp.databinding.ActivityCameraBinding
 import java.io.ByteArrayOutputStream
+
 
 
 class camera : AppCompatActivity() {
@@ -46,9 +45,18 @@ class camera : AppCompatActivity() {
           /*  binding.Upload.setOnClickListener {
                 val intent = Intent(this, Sign_up::class.java)
             startActivity(intent)}*/
-
+            /*val context = pic_id
+            val bitmap = photo
+            val ImageUri= bitmap?.let { getImageUri(context, it) }*/
         }
     }
+   /* fun getImageUri(inContext: Int, inImage: Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path =
+            MediaStore.Images.Media.insertImage(inContext.contentResolver, inImage, "Title", null)
+        return Uri.parse(path)
+    }*/
 
 
 
